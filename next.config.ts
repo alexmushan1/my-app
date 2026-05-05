0483e39e197008ee2d5f851e59f921f940b2b19d
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2026-05-05 14:02:04 -0700

    try fix deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
+const basePath = process.env.NODE_ENV === 'production' ? '/my-app' : '';
+const assetPrefix = process.env.NODE_ENV === 'production' ? '/my-app' : '';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/my-app',
-  assetPrefix: '/my-app',
+  basePath,
+  assetPrefix,
 };
-
 export default nextConfig;
